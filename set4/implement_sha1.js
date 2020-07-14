656ba9074d0be9430b90767fd44f2a08030db79a
Aditya Harit <adityah@uci.edu>
2020-07-13 20:37:58 -0600

    Implemented (horribly) sha 1 in js

--- set4/implement_sha1.js
+++ set4/implement_sha1.js
@@ -6,7 +6,7 @@ function createSHA1() {
     this.uint32 = require('uint32');
 
     this.leftRotate = function leftRotate(n, d) {
-        return ((n << d) | (n >> (32 - d))) >>> 0
+        return this.uint32.rotateLeft(n, d);
     }
 
     this.encrypt = function encrypt(mesg) {
@@ -32,54 +32,53 @@ function createSHA1() {
 
 
         let bit64ml =  [0,0,0,0,(ml >> 24), ((ml <<8) >> 24 ), ((ml << 16) >> 24), ((ml << 24) >> 24)].map(ch => String.fromCharCode(ch)).join('')
-        bit64ml
         mesg = Buffer.concat([Buffer.from(mesg), Buffer.from(bit64ml)])
-        Uint16Array
+
         
         for (let k=0; k<mesg.length; k+=64) {
             let chunk = mesg.slice(k, k+64)
             let words = Array(80)
             for (let j=0; j<64; j+=4) {
-                let word = this.uint.fromBytesBigEndian(chunk[j], chunk[j+1], chunk[j+2], chunk[j+3])
+                let word = this.uint32.fromBytesBigEndian(chunk[j], chunk[j+1], chunk[j+2], chunk[j+3])
                 words[j/4] = word
             }
-            words
 
             for (let j=16; j<=79; j+=1 ) {
                 words[j] = this.leftRotate(words[j-3] ^ words[j-8] ^ words[j-14] ^ words[j-16], 1)
             }
 
-            let a = h0
-            let b = h1
-            let c = h2
-            let d = h3
-            let e = h4
+            let a = this.uint32.toUint32(h0)
+            let b = this.uint32.toUint32(h1)
+            let c = this.uint32.toUint32(h2)
+            let d = this.uint32.toUint32(h3)
+            let e = this.uint32.toUint32(h4)
             
           
             for (let i=0; i<80; i+=1) {
-                let k = 0
-                let f = 0
+                let k = this.uint32.toUint32(0)
+                let f = this.uint32.toUint32(0)
     
-                if (i <= 19) {
-                    f = (b & c) | (~b & d)
+                if (i >= 0 && i <= 19) {
+                    f = this.uint32.toUint32((b & c) | (~b & d))
                     k = 0x5A827999
-                } else if (i <= 39) {
-                    f = b ^ c ^ d
+                } else if (i >= 20 && i <= 39) {
+                    f = this.uint32.toUint32(b ^ c ^ d)
                     k = 0x6ED9EBA1
-                } else if (i <= 59) {
-                    f = (b & c) | (b & d) | (c & d)
+                } else if (i >= 40 && i <= 59) {
+                    f = this.uint32.toUint32((b & c) | (b & d) | (c & d))
                     k = 0x8F1BBCDC
                 } else {
-                    f = b ^ c ^ d
+                    f = this.uint32.toUint32(b ^ c ^ d)
                     k = 0xCA62C1D6
                 }
 
-                temp = this.leftRotate(a,5) + f + e + k + words[i]
+                temp = (this.leftRotate(a,5) + f + e + k + words[i])  & 0xFFFFFFFF
                 e = d
                 d = c
-                c = this.leftRotate(b, 30)
+                c = this.leftRotate(b, 30) & 0xFFFFFFFF
                 b = a
                 a = temp
+
             }
 
             h0 = h0 + a
@@ -99,8 +98,12 @@ function createSHA1() {
 }
 
 let s = new createSHA1()
-console.log(s.encrypt("Hello World").toString(16))
+console.log(s.encrypt("Hello World").toString(16).padStart(40, 0))
+58814527086678835124646218365765974827431397072
+58814527086678835124646218365765974827431397072
+
 
 let sha = require("crypto").createHash("sha1")
 sha.update("Hello World")
-console.log(sha.digest("Hello World", "hex"))
+let libsha = sha.digest("Hello World", "hex")
+console.log(Buffer.from(libsha).toString('hex'))
